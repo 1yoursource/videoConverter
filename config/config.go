1f19c33f9546cac21190fa4658bd92e94e67dfe4
v.mahrelo <leo00pobeda@gmail.com>
2020-12-23 17:22:11 +0200

    first

---
+++ config/config.go
@@ -0,0 +1,39 @@
+package config
+
+// Config structure that holds Config data
+type Config struct {
+	// FILES struct {
+	// 	TEMPLATES string
+	// }
+
+	DB struct {
+		HOST  string
+		NAME  string
+		USER  string
+		PASS  string
+		DEBUG bool
+	}
+	HTTP struct {
+		HOSTNAME      string
+		LISTENADDRESS string
+	}
+}
+
+// LoadConfig fills Config struct with file data
+//func LoadConfig(log alog.Logger, config *Config) {
+//
+//	hostname, err := os.Hostname()
+//	if err != nil {
+//		log.Fatal("Cannot read hostname: ", err)
+//	}
+//	hostname = strings.ToLower(hostname)
+//
+//	if err := readConfig("./conf/"+hostname+".config.ini", config); err != nil {
+//		log.Fatal("Cannot read config file: ", err)
+//	}
+//	// log.Info("Loaded config.ini")
+//	// log.Debug("%#v\n\n", &config)
+//}
+/*func readConfig(file string, config *Config) error {
+	return gcfg.ReadFileInto(config, file)
+}*/
